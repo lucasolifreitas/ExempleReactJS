994513055d375d9fd74394b10f53425b16a06a92
Lucasolifreitas <lucasolifreitas@outlook.com>
2019-09-25 17:46:50 -0300

    Exemplo React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from './pages/main';
+import Routes from './routes'
+import './styles.css';
+import Header from './components/Header';
 
+
+const App = () =>(
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
+
+/*
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello Lucas</h1>
     </div>
   );
 }
-
+*/
 export default App;
